d95e135173f5b9339ad242da2769b02d2280e996
dtzr-code <derricktay17@gmail.com>
2021-07-06 15:47:13 +0800

    heroku

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,8 +7,8 @@ const firebaseConfig = {
     storageBucket: "ecommerce-b1ad2.appspot.com",
     messagingSenderId: "816377875665",
     appId: "1:816377875665:web:c7d8938714f64eb18449f0"
-  };
-
+};
+  
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
